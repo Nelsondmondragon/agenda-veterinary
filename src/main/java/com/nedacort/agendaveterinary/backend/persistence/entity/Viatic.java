32aeee57a26b841bd7d8f063c6c73cebc34eaa4c
Nelson Cortes <nelsondmondragon@gmail.com>
2021-08-11 18:17:42 -0500

    Mapping of Viatic

---
+++ src/main/java/com/nedacort/agendaveterinary/backend/persistence/entity/Viatic.java
@@ -0,0 +1,32 @@
+package com.nedacort.agendaveterinary.backend.persistence.entity;
+
+import com.helger.commons.annotation.IsLocked;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "viatico")
+public class Viatic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_viatico")
+    private Integer id;
+
+    @Column(name = "dinero_sobrante")
+    private Double RemainingMoney;
+
+    @Column(name = "costo_flete")
+    private Double costFreight;
+
+    @Column(name = "costo_gasoline")
+    private Double costGasoline;
+
+    @Column(name = "costo_extra")
+    private Double costExtra;
+
+    @Column(name = "costo_total")
+    private Double costTotal;
+}
